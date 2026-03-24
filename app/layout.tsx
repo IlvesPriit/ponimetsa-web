b6261b369e79a9d1087d12755cc7bbe0e0ae50a3
Priit Ilves <ilvespriit@gmail.com>
2026-03-24 18:54:30 +0200

    share og pic fi23

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,30 +4,28 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   metadataBase: new URL("https://ponimetsa.ee"),
   title: "Ponimetsa Tall",
-  description: "🦄Tall Pärnu küljel, kus kohtuvad lõbu, professionaalsus ja personaalne lähenemine!",
-
+  description: "🐴 Tall Pärnu külje, kus kohtuvad lõbu, professionaalsus ja personaalne lähenemine!",
   openGraph: {
     title: "Ponimetsa Tall",
-    description: "🦄Tall Pärnu küljel, kus kohtuvad lõbu, professionaalsus ja personaalne lähenemine!",
+    description: "🐴 Tall Pärnu külje, kus kohtuvad lõbu, professionaalsus ja personaalne lähenemine!",
     url: "https://ponimetsa.ee",
     siteName: "Ponimetsa Tall",
     locale: "et_EE",
     type: "website",
     images: [
       {
-        url: "/images/og-image.png",
+        url: "https://ponimetsa.ee/images/og-image.jpeg",
         width: 1200,
         height: 630,
         alt: "Ponimetsa Tall",
       },
     ],
   },
-
   twitter: {
     card: "summary_large_image",
     title: "Ponimetsa Tall",
-    description: "🦄Tall Pärnu küljel, kus kohtuvad lõbu, professionaalsus ja personaalne lähenemine!",
-    images: ["/images/og-image.png"],
+    description: "🐴 Tall Pärnu külje, kus kohtuvad lõbu, professionaalsus ja personaalne lähenemine!",
+    images: ["https://ponimetsa.ee/images/og-image.jpeg"],
   },
 };
 
